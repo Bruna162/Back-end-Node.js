828e516f4a6fcfcfe6388e2e515e481a925df9a6
matheusmp3 <matheussparaiso11@gmail.com>
2025-06-13 14:55:05 -0300

    .

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import AnimeS from "./service/AnimeS.js"
-import UsuarioS from "./service/UsuarioS.js"
 
 const app = express();
 
@@ -10,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/" , AnimeS)
-app.use("/" , UsuarioS )
+
 
 app.listen(3000, () => {
   console.log('Servidor rodando na porta http://localhost:3000/');
